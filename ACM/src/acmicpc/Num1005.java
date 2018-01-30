87179f3bbf64b633905ff9a7557b4cfef3e5faf8
choongyon.lee <choongyon.lee@bankwareglobal.com>
2018-01-30 17:48:56 +0900

    a

--- ACM/src/acmicpc/Num1005.java
+++ ACM/src/acmicpc/Num1005.java
@@ -6,38 +6,39 @@ import java.util.Scanner;
 
 /**
  * @author lee
- *
- * @desc
- * 	1. 건설순서 X Y 는 자료구조 어떤 것을 사용해야하는가?
- * 		- String
- * 		- Map
- * 		- 객체안에 담아두고 객체를 List 에 담기
+ * @point
+ * 	1. 건설순서 자료구조를 어떤걸 사용할 것인가?
+ * 	2. 순서를 정한 뒤 시간을 구해야하는데 시간은 최대 시간을 구해야한다. 이를 어떻게 해결할 것인가?
+ * 	3. 컴퓨터가 인식할 수 있는 문제의 패턴이 뭘까?   
  */
 public class Num1005
 {
+	private static int D[] = null;
+	private static int cache[] = null;
+	private static List<BuildingOrder> list;
+	
 	public static void main(String [] args)
 	{
 		Scanner sc = new Scanner(System.in);
 		int T = sc.nextInt();
-		int D[];
 		int W;
-		List<BuildingOrder> list;
+		
 		BuildingOrder buildingOrder = null;
 		for (int i = 0 ; i < T ; i++)
 		{
+			// 건물 및 순서 갯수
 			int N = sc.nextInt();
 			int K = sc.nextInt();
 
 			// 건설에 걸리는 시간 D
+			D = new int[N];
 			for (int j = 0 ; j < N ; j++)
 			{
-				D = new int[N];
 				D[j] = sc.nextInt();
 			}
 			
 			// 건설순서 X Y
 			list = new ArrayList<BuildingOrder>();
-			
 			for (int z = 0 ; z < K ; z++)
 			{
 				buildingOrder = new BuildingOrder(sc.nextInt(), sc.nextInt());
@@ -45,38 +46,34 @@ public class Num1005
 			}
 			
 			W = sc.nextInt();
-
+			cache = new int[N];
+			
+			System.out.println(solve(W, D, list, cache));
 		}
 	}
 	
-	public static int solve(int W, int D[], List<BuildingOrder> list)
+	public static int solve(int W, int D[], List<BuildingOrder> list, int cache[])
 	{
-		int time = 0;
+		// 기저사례
+		if (cache[W-1] != 0)
+		{
+			return cache[W-1];
+		}
+		cache[W-1] = D[W-1];
 		
+		// 재귀
 		for (BuildingOrder buildingOrder : list)
 		{
+			int time = D[W-1];
 			if (W == buildingOrder.Y)
 			{
-				time += D[buildingOrder.Y] + solve(buildingOrder.X, D, list);
+				time += solve(buildingOrder.X, D, list, cache);
+				cache[W-1] = Math.max(time, cache[W-1]); 
 			}
 		}
-		// 1. W 조건으로 앞의 순서 건물명 가져오기.
-		// 2. 가져온 건물명의 소요시간 +
-		// 3. 1~2번 반복
-		
-		return time;
-	}
-	
-	public static void print(List<BuildingOrder> list)
-	{
-		for (BuildingOrder buildingOrder : list)
-		{
-			System.out.println(buildingOrder.X + " " + buildingOrder.Y);
-		}
+		return cache[W-1];
 	}
 }
-
-
 
 class BuildingOrder
 {
