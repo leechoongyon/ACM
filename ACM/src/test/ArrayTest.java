9ae26795c26bda977718547c242f0de8feaf0902
choongyon.lee <choongyon.lee@bankwareglobal.com>
2018-03-07 22:48:58 +0900

    a

--- ACM/src/test/ArrayTest.java
+++ ACM/src/test/ArrayTest.java
@@ -3,7 +3,7 @@ package test;
 public class ArrayTest
 {
 	public static void main(String[] args)
-	{
+	{ 
 		int temp[][] = new int[5][4];
 		System.out.println(temp.length);
 		System.out.println(temp[1].length);
