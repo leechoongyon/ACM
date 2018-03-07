5f62de705c2f4dac818f0154d9c6434527abaa24
choongyon.lee <choongyon.lee@bankwareglobal.com>
2018-03-07 20:23:43 +0900

    ㅁ

---
+++ ACM/src/test/ComparatorTest.java
@@ -0,0 +1,14 @@
+package test;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+public class ComparatorTest {
+	public static void main(String[] args)
+	{
+		Map<String, Integer> map = new HashMap<String,Integer>();
+		List<Map.Entry<K, V>> list = new LinkedList<String, Integer>(map.entrySet());
+	}
+}
