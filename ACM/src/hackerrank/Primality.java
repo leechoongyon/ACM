24c296dafcf70496a9306dde5e4b47a25eef11ea
choongyon.lee <choongyon.lee@bankwareglobal.com>
2018-06-22 17:44:11 +0900

    ㅁㅁ

--- ACM/src/hackerrank/Primality.java
+++ ACM/src/hackerrank/Primality.java
@@ -8,7 +8,9 @@ import java.util.Scanner;
  * @desc 1과 자기 자신으로 나뉘는 수를 Prime 이라 한다.
  * ex) 1 - 1 (Not prime)
  *     2 - 1,2 (Prime)
- *     6 - 1,2,3,6 (Not prime)			
+ *     6 - 1,2,3,6 (Not prime)
+ *     		
+ * 원하는 답은 시간복잡도가 O(루트 n) 이거나 / 정렬 알고리즘이 O(n)	
  */
 
 public class Primality
@@ -27,7 +29,16 @@ public class Primality
 	
 	public static String solve(int n)
 	{
-		// result : Not prime or Prime 
+		// result : Not prime or Prime
+		/**
+		 * 12가 들어왔어. Prime 인지 어떻게 판단할까?
+		 * 
+		 * 솔루션 1. 
+		 * 2부터 12까지 하나씩 나눠보기.
+		 * 이러면 O(n) 임. 들어오는 수는 n 개일테니 최종 시간복잡도는 O(n의 2승)
+		 * B : 병목으로 따지면 for 문이 2번 돈다는거. 			  
+		 * U : 하나라도 자기자신 이외의 값이 나오면 break. 물론 마지막에 답이 나오면 시간복잡도는 그대로
+		 */
 		return null;
 	}
 }
