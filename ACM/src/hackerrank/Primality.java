1d369e55d192d3a5c0899abee53461d277322d3d
choongyon.lee <choongyon.lee@bankwareglobal.com>
2018-06-22 17:33:51 +0900

    업데이트

--- ACM/src/hackerrank/Primality.java
+++ ACM/src/hackerrank/Primality.java
@@ -1,6 +1,24 @@
 package hackerrank;
 
+import java.util.Scanner;
+
 public class Primality
 {
-
+	public static void main(String [] args)
+	{
+		 Scanner in = new Scanner(System.in);
+	     int p = in.nextInt();
+	     for (int pItr = 0 ; pItr < p ; pItr++)
+	     {
+	    	 int n = in.nextInt();
+	    	 String result = solve(n);
+	    	 System.out.println(result);
+	     }
+	}
+	
+	public static String solve(int n)
+	{
+		// result : Not prime or Prime 
+		return null;
+	}
 }
