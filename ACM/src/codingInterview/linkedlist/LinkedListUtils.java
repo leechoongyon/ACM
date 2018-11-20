025dc134b95877f639e660265b9cc9b52e1234bd
choongyon.lee <choongyon.lee@bankwareglobal.com>
2018-11-20 15:17:28 +0900

    ㅁ

--- ACM/src/codingInterview/linkedlist/LinkedListUtils.java
+++ ACM/src/codingInterview/linkedlist/LinkedListUtils.java
@@ -11,10 +11,10 @@ public class LinkedListUtils
 		Node tail = null;
 		while (head != null)
 		{
-			curNode = head.next;
-			head.next = tail;
-			tail = head;
-			head = curNode;
+			curNode = head.next; // 현재 노드 보존
+			head.next = tail;	// 뒤집기
+			tail = head;	// 위에서 뒤집은걸 누적해서 tail 에 붙임.
+			head = curNode;	// 다음에 빼올 값을 head 에 넣어둠.
 		}
 		return tail;
 	}
