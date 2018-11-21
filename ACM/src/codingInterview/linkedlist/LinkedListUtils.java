40eed64d5093a7a595b13852150ecbaffd138684
choongyon.lee <choongyon.lee@bankwareglobal.com>
2018-11-21 14:41:41 +0900

    a

--- ACM/src/codingInterview/linkedlist/LinkedListUtils.java
+++ ACM/src/codingInterview/linkedlist/LinkedListUtils.java
@@ -18,4 +18,62 @@ public class LinkedListUtils
 		}
 		return tail;
 	}
+	
+	public static Node reverseAndClone(Node node)
+	{
+		Node head = null;
+		while (node != null)
+		{
+			Node n = new Node(node.data);
+			n.next = head;
+			head = n;
+			node = node.next;
+		}
+		return head;
+	}
+	
+	
+	private static Node solve(Node node)
+	{
+		Node curNode = null;
+		Node tail = null;
+		while (node != null)
+		{
+			curNode = node.next;
+//			System.out.println("curNode = node.next; : " + curNode);
+			node.next = tail;
+//			System.out.println("node.next = tail : " + node.next);
+			tail = node;
+//			System.out.println("tail = node : " + tail);
+			node = curNode;
+//			System.out.println("node = curNode : " + node);
+//			System.out.println();
+		}
+		return tail;
+	}
+	
+	private static Node solve1(Node node)
+	{
+		Node tail = null;
+		while (node != null)
+		{
+			Node n = new Node(node.data);
+			n.next = tail;
+			tail = n;
+			node = node.next;
+		}
+		return tail;
+	}
+	
+	public static void main(String [] args)
+	{
+		Node node = new Node(1);
+		node.appendTail(2);
+		node.appendTail(3);
+		node.appendTail(4);
+		node.appendTail(5);
+		System.out.println(node);
+		System.out.println(solve(node));
+//		System.out.println(solve1(node));
+	}
 }
--- ACM/src/codingInterview/linkedlist/Problem2f.java
+++ ACM/src/codingInterview/linkedlist/Problem2f.java
@@ -1,7 +1,5 @@
 package codingInterview.linkedlist;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 import java.util.Stack;
 
@@ -30,77 +28,12 @@ public class Problem2f
 			else
 				node.appendTail(scan.next());
 		}
-		boolean result = solve2(node);
+		boolean result = solve1(node);
 		System.out.println(result);
-	}
-	
-	private static boolean solve(Node node)
-	{
-		/**
-		 * sol1
-		 * 1. NodeList 에 있는 것을 배열로 옮김.
-		 * 2. 배열의 처음과 끝을 비교. 하나씩 더하거나 빼면서 서로 비교.
-		 * 3. 길이가 홀수, 짝수 일 때 나눠서 처리
-		 * 
-		 * 
-		 * sol2
-		 * 1. head node, end node 를 둔다.
-		 * 2. head node 와 end node 의 값을 비교하면서 내려옴.
-		 * 3. 종료 조건은 node 의 length / 2 를 초과했는지 안했는지 판단.
-		 * 
-		 * ex) 1 2 3 4 5 4 3 2 1 (9 - 홀수) 라면 마지막까지 비교할필요 없음.
-		 * 	   1 2 3 4 5 5 4 3 2 1 (10 - 짝수) 라면 마지막까지 비교해야 함. 
-		 */
-		List list = new ArrayList();
-		while (node != null)
-		{
-			list.add(node.data);
-			node = node.next;
-		}
-		
-		Object obj[] = list.toArray();
-		int start = 0;
-		int end = list.size() - 1;
-		// 홀수
-		if (list.size() % 2 == 1)
-		{
-			while (start != end)
-			{
-				if ( !obj[start].equals(obj[end]))
-					return false;
-				start++; end--;
-			}
-		}
-		// 짝수
-		else 
-		{
-			while (start < end)
-			{
-				if ( !obj[start].equals(obj[end]))
-					return false;
-				start++; end--;
-			}
-		}
-		return true;
 	}
 	
 	private static boolean solve1(Node node)
 	{
-		/**
-		 * 
-		 * * 길이를 입력 값으로 받지 않는다 가정.
-		 * 
-		 * 1. 절반을 탐색?
-		 * 		- 홀수, 짝수 인지 구별
-		 * 2. 뒤에 절반을 뒤집어서 LinkedList 에 집어넣는다.
-		 * 3. 비교한다.
-		 * 
-		 * 
-		 * 1 2 3 4 5 5 4 3 2 1   10 / 2 = 5
-		 * 1 2 3 4 5 4 3 2 1 	  9 / 2 = 4
-		 * 
-		 */
-		
 		Node head = node;
 		Node temp = node;
 		Node midNode = node;
@@ -126,14 +59,11 @@ public class Problem2f
 		}
 		
 
-		reverse = LinkedListUtils.reverse(midNode);
-		System.out.println("head : " + head);
-
-		System.out.println("reverse : " + reverse);
+		reverse = LinkedListUtils.reverseAndClone(midNode);
 		
 		while (reverse != null)
 		{
-			if (head.data != reverse.data)
+			if ( !head.data.equals(reverse.data))
 				return false;
 			reverse = reverse.next;
 			head = head.next;
