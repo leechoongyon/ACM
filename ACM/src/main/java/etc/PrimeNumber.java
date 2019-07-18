c20cfbf9b1d81847e579ea929a336360deb2fbc4
choongyon.lee <choongyon.lee@bankwareglobal.com>
2019-07-18 16:47:28 +0900

    a

---
+++ ACM/src/main/java/etc/PrimeNumber.java
@@ -0,0 +1,41 @@
+package etc;
+
+public class PrimeNumber {
+	public static void main(String[] args) {
+		System.out.println(isPrime(2));
+		System.out.println(isPrime(3));
+		System.out.println(isPrime2(21));
+	}
+	
+	/** time complexity : O(n) */
+	public static boolean isPrime(int n) {
+		if (n == 2) {
+			return true;
+		}
+		for (int i =  2 ; i < n ; i++) {
+			if (n % i == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	/** 
+	 * 에라토네스의 접근
+	 * 주어진 num 을 특정 수로 나누면 몫을 구할 수 있다.
+	 * 몫은 root num 보다 크거나 작거나 같다. 그렇기에 소수라면
+	 * root num 이하의 작은 수들에 의해 나누어지면 안된다.
+	 * time complexity : O(root n)
+	 **/
+	public static boolean isPrime2(int n) {
+		if (n == 2) {
+			return true;
+		}
+		for (int i = 2 ; i < Math.sqrt(n) ; i++) {
+			if (n % i == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
